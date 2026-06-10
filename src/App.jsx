73f976d5f522c78b9c5c7359b7542f1556d5c917
0980596 <ds41216950890@gmail.com>
2026-06-09 23:24:39 -0300

    feat: add Lenis smooth scroll and configure global styles

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,24 @@
+import { useEffect } from "react";
+import Lenis from "lenis";
 import "./App.css";
 import RoutesApp from "./routes";
 
 function App() {
+  useEffect(() => {
+    const lenis = new Lenis();
+
+    function raf(time) {
+      lenis.raf(time);
+      requestAnimationFrame(raf);
+    }
+
+    requestAnimationFrame(raf);
+
+    return () => {
+      lenis.destroy();
+    };
+  }, []);
+
   return (
     <RoutesApp />
   );
