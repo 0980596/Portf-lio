44fdb6e8d1dda5be3ad210078c54848059474249
0980596 <ds41216950890@gmail.com>
2026-07-01 13:05:17 -0300

    fix: alterar ./pages/home para ./pages/Home

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import Home from "./pages/home";
+import Home from "./pages/Home";
 import Lenis from "lenis";
 import { useEffect } from "react";
 
